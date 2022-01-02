c5873cbb795e15c70c45e4b21937b391b33ccd3c
kartik-malik <kartikmalikboss@gmail.com>
2022-01-02 13:33:56 +0530

    finalization

--- src/components/Layout/MainNav.js
+++ src/components/Layout/MainNav.js
@@ -14,7 +14,7 @@ const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <Link to="/">
-        <div className={classes.logo}>React Auth</div>
+        <div className={classes.logo}>Advertisement</div>
       </Link>
       <nav>
         <ul>
